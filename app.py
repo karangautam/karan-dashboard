aac5c9397c2781fce182ffdeaf01a720c803009e
Karan Singh Gautam <gautam_karan_singh@hotmail.com>
2019-06-13 09:31:45 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -10,9 +10,42 @@ app = dash.Dash()
 server = app.server
 app.config.supress_callback_exceptions = True
 rfm = pd.read_csv('rfm.csv')
+
+
+
 app.layout = html.Div([
-    dcc.Graph(id='treemap')
+    html.Div(
+        className="row",
+        children=[
+            html.Div(
+                className="six columns",
+                children=[
+                    html.Div(
+                        children=dcc.Graph(
+                            id='treemap'   
+                        )
+                    )
+                ]
+            ),
+            html.Div(
+                className="six columns",
+                children=html.Div([
+                    dcc.Graph(
+                        id='my-graph'
+                    ),
+                    dcc.Graph(
+                        id='explore'
+                    ),
+
+                ])
+            )
+        ]
+    )
 ])
+
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
 
 @app.callback(
     Output('treemap', 'figure'),
@@ -83,6 +116,106 @@ def treemap(value):
         )
     }
     return figure
+
+@app.callback(
+    Output("my-graph", "figure"),
+    [Input('my-graph', 'value')])
+
+def ugdate_figure(value):
+    z = rfm['Frequency']
+    trace = [go.Scatter3d(
+        x=rfm['Recency'], y=rfm['Frequency'], z=rfm['Monetary'],
+        mode='markers', marker={'size': 8 })]
+    return {"data": trace,
+            "layout": go.Layout(
+                height=700, title="Recency Frequency Monetary",
+                paper_bgcolor="#f3f3f3",
+                scene={"aspectmode": "cube", "xaxis": {"title": "Recency" },
+                       "yaxis": {"title": "Frequency" },
+                       "zaxis": {"title": "Monetary" }})
+            }
+
+@app.callback(
+    Output('explore', 'figure'),
+    [Input('explore', 'value')])
+def explor(value):
+    data=pd.read_csv("customer.csv")
+    Annual_Income=data.iloc[:,3]
+    Spending_Score=data.iloc[:,4]
+    Age=data.iloc[:,2]
+    Gender=data.iloc[:,1]
+    Index=data.iloc[:,0]
+
+    trace1 = go.Scatter(
+        x=Spending_Score,
+        y=Gender,
+        name = "Gender",
+        mode = "markers"
+    )
+    trace2 = go.Scatter(
+        x=Spending_Score,
+        y=Age,
+        xaxis='x2',
+        yaxis='y2',
+        name = "Age",
+        mode = "markers"
+    )
+    trace3 = go.Scatter(
+        x=Spending_Score,
+        y=Annual_Income,
+        xaxis='x3',
+        yaxis='y3',
+        name = "income",
+        mode = "markers"
+    )
+    trace4 = go.Scatter(
+        x=Spending_Score,
+        y=Index,
+        xaxis='x4',
+        yaxis='y4',
+        name = "total_score",
+        mode = "markers"
+    )
+    data = [trace1, trace2, trace3, trace4]
+    layout = go.Layout(
+        xaxis=dict(
+            domain=[0, 0.45]
+        ),
+        yaxis=dict(
+            domain=[0, 0.45]
+        ),
+        xaxis2=dict(
+            domain=[0.55, 1]
+        ),
+        xaxis3=dict(
+            domain=[0, 0.45],
+            anchor='y3'
+        ),
+        xaxis4=dict(
+            domain=[0.55, 1],
+            anchor='y4'
+        ),
+        yaxis2=dict(
+            domain=[0, 0.45],
+            anchor='x2'
+        ),
+        yaxis3=dict(
+            domain=[0.55, 1]
+        ),
+        yaxis4=dict(
+            domain=[0.55, 1],
+            anchor='x4'
+        ),
+        title = 'Spending_Score'
+    )
+    fig = go.Figure(data=data, layout=layout)
+
+    return fig
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
